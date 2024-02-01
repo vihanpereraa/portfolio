5a71fed8279c773c8d3657896d3b75d949628a56
vihanpereraa <vihanperera334@gmail.com>
2024-01-31 21:56:44 -0500

    nav bar updates

---
+++ src/components/NavBar.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export  const NavBar = () => {
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, seScrolled] = useState(false);
+
+useEffect(() => {
+    const onScroll = () => {
+        if (window.scrollY > 50) {
+            seScrolled(true);
+        }   else {
+            seScrolled(false)
+        }
+    }
+}, [])
+
+
+    return(
+function BasicExample() {
+    
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+                <Navbar.Brand href="#home">Home</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#skills">Skills</Nav.Link>
+                    <Nav.Link href="#projects">Projects</Nav.Link>
+                    <Nav.Link href="#experience">Experience</Nav.Link>
+                </Nav>
+                <span className="navbar-text">
+                    <div className="social-icon">
+                        <a href="#"><img src={} alt="" /></a>
+                        <a href="#"><img src={} alt="" /></a>
+                        <a href="#"><img src={} alt="" /></a>
+                    </div>
+                    <button className="vvd" onClick={() => console.log('connect')} ><span>Let's connect!</span></button>
+                </span>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+  );
+}
+
+)
+
+}
